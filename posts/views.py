1eead2ede93f8fb288169dcb43a50613df16ffe7
beniman8 <benik@live.ca>
2021-03-25 22:58:13 -0400

    setting up a basic page and a few apps

---
+++ posts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home_view(request):
+    context = {
+        'hello':'hello world',
+    }
+    return render(request,'posts/main.html',context)
