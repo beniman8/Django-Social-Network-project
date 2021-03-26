0dcf5dcca046cdc7632ba0e64bf15770bd9d9e55
beniman8 <benik@live.ca>
2021-03-26 00:31:08 -0400

    made an error with static root by naming it media

--- social_core/settings.py
+++ social_core/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     
     BASE_DIR / 'posts' / 'static',
